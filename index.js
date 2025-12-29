41d69ce252d374442350c7e1a71ec72f9b026698
Madhina-18 <madhinabanu7866@gmail.com>
2025-12-29 15:56:26 +0530

    Final try

---
+++ index.js
@@ -0,0 +1,105 @@
+// Javascript Master //
+
+//1.What are variables//
+
+//Let Example//
+
+// let a = 20 
+// console.log(a)
+
+
+//Var Example//
+// var b = "Madhina"
+// console.log(b)
+
+//const Example//
+// var days = 7
+// console.log(days)
+
+//2.Es6 means 
+
+// * Arrow function , 
+// Block scope variable (LVC) , 
+// Template Literals , 
+// Object Literals , 
+// Destructuring assignment, 
+// default parameters ,
+//  rest and spread opertors , 
+//  classes , 
+// asyn/await //
+
+//3.js primitivies//
+//*String
+// boolean
+// number
+// null
+// Undefined..//
+
+
+//4.js opertors //
+//* Arithmatic opertors (+,-,*,%)
+//*Comparision operator  (a<b,a<b,falsror true)
+
+//Logical oprator
+//6.Logical opertor //
+//true & true = true
+// true & false = false 
+// false & false =  false
+// true || false = true 
+// false || false = false 
+
+
+//assignment oprator (var a"=" 10)
+//Increment and Decrement oprator (a++,a--)
+//Condition or ternary operator ()
+
+
+//  var a = "EMC" == "madhina" ? "Both are Equal" : "Not same"
+// {
+//     console.log(a)
+// }
+
+// var b = 10>8 ? "True" : "False"
+// {
+//     console.log(b)
+// }
+
+//5.If else //
+// if("EMC" == "EMC")
+// {
+//     console.log("same")
+// }
+
+// else{
+//     console.log("Not same")
+// }
+
+//6.function //
+
+// function madhi()
+// {
+//    console.log(10+80)
+// }
+
+// madhi()
+
+//7.Parameters//
+// function a (a,b)
+// {
+//     console.log (a+b)
+// }
+
+// a(10,90)
+
+
+//return keyword//
+// function myname()
+// {
+//     return "Madhina"
+// }
+
+// var a = myname()
+
+// console.log(a)
+
+
