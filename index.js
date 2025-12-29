51fd5765541d7efed4e4de6577c60c02da73fbd7
Madhina-18 <madhinabanu7866@gmail.com>
2025-12-29 17:24:02 +0530

    js updated qs

--- index.js
+++ index.js
@@ -92,7 +92,7 @@
 // a(10,90)
 
 
-//return keyword//
+//8return keyword//
 // function myname()
 // {
 //     return "Madhina"
@@ -103,3 +103,45 @@
 // console.log(a)
 
 
+//9.Math.random
+// var a = Math.random()*10
+// console.log(a)
+
+// //10.Math.floor//
+// var b = Math.random()*10
+// console.log(Math.floor(b)+1)
+
+//10.loops//
+// for(count=1;count<10;count=count+1)
+// {
+//     console.log("Sorry")
+// }
+
+
+//11.What is array//
+// var a = [10,20,30,40]
+
+// console.log(a)
+
+//push method - add value of the element in the last place//
+// var a = [10,20,30,40]
+// a.push(50)
+// console.log(a)
+
+//pop method - remove the element value of the last place//
+// var a = [1,2,3,4,5]
+// a.pop()
+// console.log(a)
+
+//splice method - help to pick the particular value //
+// var a = [1,2,3,4,5]
+// var b = a.slice(0,3) //starting position And ending position 
+
+// console.log(b) //printed the starting and ending before the the element value
+
+//splice method - remove particular element //
+
+// var a = [10,20,30,40]
+// a.splice(2,1)
+
+// console.log(a)
